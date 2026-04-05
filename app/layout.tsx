c419cf1a5396606c0fb23d3df2a7420004b22371
Galib24 <abuyeahia24@gmail.com>
2026-04-05 13:32:05 +0600

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,4 @@
-// import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
-// import "./globals.css";
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
-
-// export const metadata: Metadata = {
-//   title: "Yellow Page Bangladesh",
-//   description: "It's a yellow page website for bangladesh for search all kind of business.",
-// };
-
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   return (
-//     <html
-//       lang="en"
-//       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-//     >
-//       <body suppressHydrationWarning className="min-h-full flex flex-col">
-//         {children}
-//       </body>
-//     </html>
-//   );
-// }
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -54,23 +20,6 @@ export const metadata: Metadata = {
     icon: "https://i.postimg.cc/x8n10xYf/yellow.png",
   },
 };
-
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   return (
-//     <html
-//       lang="en"
-//       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-//     >
-//       <body suppressHydrationWarning className="min-h-full flex flex-col">
-//         {children}
-//       </body>
-//     </html>
-//   );
-// }
 
 
 export default function RootLayout({
